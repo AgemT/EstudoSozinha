aabc7e47dd1b79fff07e1564827080f1e104fd86
AnaBatista <144141675+AgemT@users.noreply.github.com>
2024-09-25 11:19:39 -0300

    Add files via upload

---
+++ tantrum.php
@@ -0,0 +1,3 @@
+<?php 
+require("objetos.php");
+$mycar = new Pessoa;
---
+++ walmonn.py
@@ -0,0 +1,8 @@
+#quando fizer um input voce precisa declarar o tipo da variavel. Eu te amo Ana Clara <3
+nome = input("Digite seu nome: ")
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+print(nome)
+print(n1+n2)
+import os
+os.system("clear")
